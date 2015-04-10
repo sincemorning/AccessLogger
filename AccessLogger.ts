eec565009146359cf0c567bf37e85511b8109f49
takumi.hoshi <takumi.hoshi@carview.co.jp>
2015-04-10 19:10:03 +0900

    要素のリストが取得できなかった問題の修正

--- AccessLogger.ts
+++ AccessLogger.ts
@@ -17,12 +17,14 @@ class AccessLoggerItems {
 
 /**
  * アクセスログ仕込み用
+ *  document.getElementByTagNameがFireFoxでは正常に動かない
  */
 class AccessLogger extends AccessLoggerItems {
 	/*このライブラリが対応するすべての要素にアクセスロガーを仕込む*/
 	SetLoggerToThisPage() {
 		this.SetLoggerToButtonThisPage();
 	}
+
 
 	/*読み込んだページのinput type="button"のクリックにアクセスロガーを設定*/
 	SetLoggerToButtonThisPage() {
@@ -31,7 +33,6 @@ class AccessLogger extends AccessLoggerItems {
 		/*対象属性の要素を洗い出し*/
 		for(var i = 0; i < ButtonTags.length; i++) {
 			alert("@@@@" + ButtonTags[i].id + ":" + ButtonTags[i].type);
-
 			/*対象属性の要素の指定のtypeを洗い出し*/
 			if (ButtonTags[i].type.toString()  == "button" ) {
 				targetObj = document.getElementById(ButtonTags[i].id.toString());
@@ -43,22 +44,21 @@ class AccessLogger extends AccessLoggerItems {
 	/*読み込んだページのinput type="text"にアクセスロガーを設定*/
 	/*読み込んだページのinput type="button"のクリックにアクセスロガーを設定*/
 	SetLoggerToInputTextThisPage() {
-		var ButtonTags = document.getElementsByTagName("input");
+		var InputTextTags = document.getElementsByTagName("input");
 		var targetObj : HTMLElement;
 		/*対象属性の要素を洗い出し*/
-		for(var i = 0; i < ButtonTags.length; i++) {
-			alert("@@@@" + ButtonTags[i].id + ":" + ButtonTags[i].type);
-
+		for(var i = 1; i <= InputTextTags.length; i++) {
+		//	alert("@@@@" + InputTextTags[i].id + ":" + InputTextTags[i].type);
 			/*対象属性の要素の指定のtypeを洗い出し*/
-			if (ButtonTags[i].type.toString()  == "text" ) {
-				targetObj = document.getElementById(ButtonTags[i].id.toString());
+			if (InputTextTags[i].type.toString()  == "text" ) {
+				targetObj = document.getElementById(InputTextTags[i].id.toString());
 				targetObj.addEventListener("change", this.SendAction, false);
 			}
 		}
 	}
-
-
 }
 
 /*ボタン要素にアクセスログを仕込む*/
-AccessLogger.prototype.SetLoggerToButtonThisPage();
+window.addEventListener("load", AccessLogger.prototype.SetLoggerToButtonThisPage, true)
+window.addEventListener("load", AccessLogger.prototype.SetLoggerToInputTextThisPage, true)
+
