ea45c8b6e99c1f4591b768a97fe8fd0e7c648761
takumi.hoshi <takumi.hoshi@carview.co.jp>
2015-04-13 12:10:28 +0900

    処理の仕込みが正常に動作していなかった問題の修正

--- AccessLogger.ts
+++ AccessLogger.ts
@@ -2,17 +2,25 @@
  * アクセスログ基本部分
  */
 class AccessLoggerItems {
-	thisUserAgent:String = window.navigator.userAgent;
-	thisURL:String = window.location.href;
-	thisUser:String = "";
-	thisIPAddress:String = "";
-	thisTargetDOM = "";
-	thisAction:String = "";
+	private thisUserAgent:String;
+	private thisURL:String;
+	private thisUser:String;
+	private thisIPAddress:String;
+	private thisTargetDOM:any;
+	private thisAction:String;
 
-	SendAction(e : MouseEvent) {
-		alert("aaa" + e.type);
+	constructor() {
+	 	this.thisUserAgent = window.navigator.userAgent;
+		this.thisURL = window.location.href;
+		this.thisUser = "";
+		this.thisIPAddress = "";
+		this.thisTargetDOM = "";
+		this.thisAction = "";
 	}
-
+	/*実際に仕込むアクション処理*/
+	public SendAction() {
+		alert("aaa");
+	}
 }
 
 /**
@@ -20,45 +28,22 @@ class AccessLoggerItems {
  *  document.getElementByTagNameがFireFoxでは正常に動かない
  */
 class AccessLogger extends AccessLoggerItems {
-	/*このライブラリが対応するすべての要素にアクセスロガーを仕込む*/
-	SetLoggerToThisPage() {
-		this.SetLoggerToButtonThisPage();
-	}
-
-
 	/*読み込んだページのinput type="button"のクリックにアクセスロガーを設定*/
-	SetLoggerToButtonThisPage() {
+	public SetLoggerToButtonThisPage() {
 		var ButtonTags = document.getElementsByTagName("input");
 		var targetObj : HTMLElement;
 		/*対象属性の要素を洗い出し*/
 		for(var i = 0; i < ButtonTags.length; i++) {
-			alert("@@@@" + ButtonTags[i].id + ":" + ButtonTags[i].type);
 			/*対象属性の要素の指定のtypeを洗い出し*/
 			if (ButtonTags[i].type.toString()  == "button" ) {
 				targetObj = document.getElementById(ButtonTags[i].id.toString());
-				targetObj.addEventListener("click", this.SendAction, false);
+				targetObj.addEventListener("click", super.SendAction, true);
 			}
 		}
 	}
 
-	/*読み込んだページのinput type="text"にアクセスロガーを設定*/
-	/*読み込んだページのinput type="button"のクリックにアクセスロガーを設定*/
-	SetLoggerToInputTextThisPage() {
-		var InputTextTags = document.getElementsByTagName("input");
-		var targetObj : HTMLElement;
-		/*対象属性の要素を洗い出し*/
-		for(var i = 1; i <= InputTextTags.length; i++) {
-		//	alert("@@@@" + InputTextTags[i].id + ":" + InputTextTags[i].type);
-			/*対象属性の要素の指定のtypeを洗い出し*/
-			if (InputTextTags[i].type.toString()  == "text" ) {
-				targetObj = document.getElementById(InputTextTags[i].id.toString());
-				targetObj.addEventListener("change", this.SendAction, false);
-			}
-		}
-	}
-}
+}/*class AccessLogger*/
 
 /*ボタン要素にアクセスログを仕込む*/
-window.addEventListener("load", AccessLogger.prototype.SetLoggerToButtonThisPage, true)
-window.addEventListener("load", AccessLogger.prototype.SetLoggerToInputTextThisPage, true)
-
+var al:AccessLogger = new AccessLogger();
+window.addEventListener("load", al.SetLoggerToButtonThisPage, false);
